8296613881dce004c06acc4d94d40c332886ac5d
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-11-15 07:27:33 -0300

    Finalizando projeto de contar letras maiúsculas e minúsculas de uma string

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 
 def main():
-    pass
+    text = "This Sentence Has Mixed CASE Letters!"
+    upper_counter = len([c for c in text if c.isupper()])
+    lower_counter = len([c for c in text if c.islower()])
+    print(f"The number of uppercase letters is: {upper_counter}")
+    print(f"The number of lowercase letters is: {lower_counter}")
 
 
 if __name__ == "__main__":
